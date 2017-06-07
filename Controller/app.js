69c4b343647de8838c94d0102bb5b1b6284a7b12
Brandon Little <brlittle86@gmail.com>
2017-06-07 11:49:04 -0700

    experimenting for benefit of a student

--- Controller/app.js
+++ Controller/app.js
@@ -1,3 +1,11 @@
 'use strict';
 
 console.log('Successfully linked with index.html');
+
+var dog = prompt('Tell me a thing');
+
+if (dog.toLowerCase() === ('potato' || 'grass')) {
+  console.log('They like the stuff');
+} else {
+  console.log('bad juju');
+}
