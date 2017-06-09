bfcf354711dc2ee26eeb754b0ea3779afafe50b5
Brandon Little <brlittle86@gmail.com>
2017-06-09 10:08:22 -0700

    learning new features of my ide

--- Controller/app.js
+++ Controller/app.js
@@ -1,3 +1,19 @@
 'use strict';
 
 console.log('Successfully linked with index.html');
+
+var awesome = true;
+
+function likeABoss1(awesomeParam1, awesomeParam2) {
+  if (awesome) {
+    console.log(awesomeParam1 + ' ' + awesomeParam2);
+  }
+}
+function likeABoss2(awesomeParam1, awesomeParam2) {
+  if (awesome) {
+    console.log(awesomeParam1 + ' ' + awesomeParam2);
+  }
+}
+
+likeABoss1('You\'re', 'cool.');
+likeABoss2('Still', 'cool.');
